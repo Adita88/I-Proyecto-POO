ddd6af830d20da52b7988b3549d48f29079edd09
Mariana y David <Usuario@Probook440G1>
2020-06-05 20:43:57 -0600

    Probando GITHub

--- src/main/java/controlador/ControladorSismo.java
+++ src/main/java/controlador/ControladorSismo.java
@@ -15,7 +15,7 @@ public class ControladorSismo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba GITHub");
+        System.out.println("Prueba GITHub ");
     }
     
 }
